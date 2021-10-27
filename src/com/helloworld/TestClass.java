eb9768e9ced74ffd76f9f801296513e5389cbbd2
huangmickey <67403573+huangmickey@users.noreply.github.com>
2021-10-27 08:33:31 -0700

    test_branch first commit

--- src/com/helloworld/TestClass.java
+++ src/com/helloworld/TestClass.java
@@ -1,2 +1,8 @@
-package com.helloworld;public class TestClass {
+package com.helloworld;
+
+public class TestClass {
+
+    public TestClass() {
+        System.out.println("THIS IS A TEST");
+    }
 }
